71ebfdbe0aec96a818156c17c75c4f1dbcf77136
Lucas Augusto <lucasaugustscode@gmail.com>
2020-12-09 22:24:13 -0300

    Update
    Tomorrow I need to refactor the code and work on the project presentation.

--- src/server.js
+++ src/server.js
@@ -1,3 +1,15 @@
-import app from './app'
+import express from 'express'
+import cors from 'cors'
 
-app.listen(3000, () => console.log('Server ready for work !'))
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+server.use(express.static('public'))
+
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+server.listen(3000, () => console.log('Server is on !'))
