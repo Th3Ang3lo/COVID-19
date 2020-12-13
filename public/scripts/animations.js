cac01f24377e33955568885335f4a6f3d5e95aa8
Lucas Augusto <lucasaugustscode@gmail.com>
2020-12-13 00:23:19 -0300

    Updates

---
+++ public/scripts/animations.js
@@ -0,0 +1,39 @@
+// search animation;
+
+const searchButton = document.querySelector('.search-button')
+const closeSearchButton = document.querySelector('.close-search-button')
+const searchBox = document.querySelector('.search-box')
+const searchInput = document.querySelector('input')
+
+
+searchButton.addEventListener('click', () => {
+    searchBox.classList.add('active')
+    searchInput.classList.add('active')
+    searchButton.classList.add('active')
+    closeSearchButton.classList.add('active')
+})
+
+closeSearchButton.addEventListener('click', () => {
+    searchBox.classList.remove('active')
+    searchInput.classList.remove('active')
+    searchButton.classList.remove('active')
+    closeSearchButton.classList.remove('active')
+    searchInput.value = ''
+})
+
+// voice button animation;
+
+
+const utterance = new SpeechSynthesisUtterance()
+const voiceButton = document.querySelector('.voice-button')
+const inputVoice = document.getElementById('voice')
+
+inputVoice.addEventListener('change', () => {
+    if (inputVoice.checked == true) {
+        voiceButton.classList.add('active')
+        utterance.text = 'Sistema de voz habilitado'
+        speechSynthesis.speak(utterance)
+    } else {
+        voiceButton.classList.remove('active')
+    }
+})
---
+++ public/scripts/map.js
@@ -0,0 +1,17 @@
+var map = L.map('mapid', {
+    center: [-20.231, -44.250],
+    zoom: 4,
+    zoomControl: false
+});
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '<strong>COVID-19</strong>',
+    maxZoom: 4,
+    minZoom: 4,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibHVjYXNhdWd1c3RzIiwiYSI6ImNraWt2cTluczBkdHAzMnFpNDkzMmp4ZHIifQ.F_7HtbNL_iCunJQ0AjR7OQ'
+}).addTo(map);
+
+
